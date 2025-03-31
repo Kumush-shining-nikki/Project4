e51b9b5bb419b34adeccdb7567e623aaf2a20ed7
Kumush-shining-nikki <kumushizziyeva@gmail.com>
2025-03-31 15:58:20 +0500

    ......

--- frontend/script.js
+++ frontend/script.js
@@ -1,37 +1,19 @@
-const carousel = document.querySelector('.carousel');
-const texts = document.querySelectorAll('.carousel p');
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
+let scrollAmount = 0;
+const scrollStep = 200;
+const carousel = document.querySelector(".carousel");
 
-let index = 0;
-
-function updateCarousel() {
-  const offset = -index * texts[0].offsetWidth;
-  carousel.style.transform = `translateX(${offset}px)`;
+function scrollLeft() {
+    scrollAmount -= scrollStep;
+    if (scrollAmount < 0) {
+        scrollAmount = 0;
+    }
+    carousel.style.transform = `translateX(-${scrollAmount}px)`;
 }
 
-rightArrow.addEventListener('click', () => {
-  if (index < texts.length - 1) {
-    index++;
-  } else {
-    index = 0;
-  }
-  updateCarousel();
-});
-leftArrow.addEventListener('click', () => {
-  if (index > 0) {
-    index--;
-  } else {
-    index = texts.length - 1;
-  }
-  updateCarousel();
-});
-
-setInterval(() => {
-  if (index < texts.length - 1) {
-    index++;
-  } else {
-    index = 0;
-  }
-  updateCarousel();
-}, 5000);
+function scrollRight() {
+    scrollAmount += scrollStep;
+    if (scrollAmount > carousel.scrollWidth - carousel.clientWidth) {
+        scrollAmount = 0;
+    }
+    carousel.style.transform = `translateX(-${scrollAmount}px)`;
+}
