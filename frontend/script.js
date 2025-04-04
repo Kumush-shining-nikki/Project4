115fbd17d2e501e74077fbf5117b5d608131ea47
Kumush-shining-nikki <kumushizziyeva@gmail.com>
2025-04-04 22:33:16 +0500

    ......

--- frontend/script.js
+++ frontend/script.js
@@ -1,19 +1,19 @@
 let scrollAmount = 0;
 const scrollStep = 200;
-const carousel = document.querySelector(".carousel");
+const carousel = document.querySelector('.carousel');
 
 function scrollLeft() {
-    scrollAmount -= scrollStep;
-    if (scrollAmount < 0) {
-        scrollAmount = 0;
-    }
-    carousel.style.transform = `translateX(-${scrollAmount}px)`;
+  scrollAmount -= scrollStep;
+  if (scrollAmount < 0) {
+    scrollAmount = 0;
+  }
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
 }
 
-function scrollRight() {
-    scrollAmount += scrollStep;
-    if (scrollAmount > carousel.scrollWidth - carousel.clientWidth) {
-        scrollAmount = 0;
-    }
-    carousel.style.transform = `translateX(-${scrollAmount}px)`;
+function scrollRight() {  
+  scrollAmount += scrollStep;
+  if (scrollAmount > carousel.scrollWidth - carousel.clientWidth) {
+    scrollAmount = 0;
+  }
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
 }
