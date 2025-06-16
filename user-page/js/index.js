839ba896beb1203b060236546df850738b12dd96
Kumush-shining-nikki <kumushizziyeva@outlook.com>
2025-06-16 16:01:24 +0500

    ..

--- user-page/js/index.js
+++ user-page/js/index.js
@@ -9,10 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       products.forEach(product => {
-        const firstImage = product.images[0]; // faqat 1-rasmni olayapti
+        const firstImage = product.images[0];
         if (firstImage) {
           const slide = document.createElement("div");
-          slide.className = "swiper-slide";
+          slide.className = "swiper-slide gallery-slide";
           slide.innerHTML = `<img src="${firstImage}" alt="product" />`;
           wrapper.appendChild(slide);
         }
@@ -33,3 +33,30 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
 });
+
+
+setTimeout(() => {
+  
+new Swiper('.gallery-slider', {
+  pagination: {
+     el: '.swiper-pagination',
+     type: 'bullets',
+     clickable: true
+  },
+  navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+  },
+  loop: false,
+  effect: 'coverflow',
+  centeredSlides: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 100,
+    depth: 150,
+    modifier: 1.5,
+    slideShadows: false,
+  }
+  });
+})
